3426dee3fdb5ccac485b54883b5f1981cca41b09
Robert336 <rmazza33@gmail.com>
2022-05-27 20:26:53 -0400

    add comments

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import csv
 
 # Main (driver code)
 
+# array containing all city objects
 cities = []
-
 
 # read data from csv file
 with open('city_data_50.csv', 'r') as file:
-    reader = csv.reader(file)
+    reader = csv.reader(file) # skip first line
+    # interate through each row in the csv file
     for row in reader:
         print(row)
+        # add city object to array
         cities.append(City(row[0], row[1], row[2], row[3]))
 
 
