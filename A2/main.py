21ac3767ce1f9207de68275e6778cd707bd7d783
Robert Mazza <rmazza33@gmail.com>
2022-06-18 12:57:22 -0400

    i think it works

--- A2/main.py
+++ A2/main.py
@@ -4,10 +4,9 @@ print("N-Queens GA")
 print("-----------------------------------------------------")
 
 num_of_queens = 8
-maxFitness = functions.maxFitness(num_of_queens)
 population_size = 100
-
 population = [ functions.generate_chromosome(num_of_queens) for i in range(population_size) ]
+maxFitness = functions.maxFitness(population[0])
 
 gen = 1
 
@@ -16,15 +15,15 @@ while not maxFitness in [ functions.fitness(chromosome) for chromosome in popula
     
     # generate new population
     for i in range(population_size):
-        x = function.select_parent(population, maxFitness) # select parent 1
-        y = function.select_parent(population, maxFitness) # select parent 2
-        child = function.cross_over(x, y) # cross over function 
+        x = functions.select_parent(population, maxFitness) # select parent 1
+        y = functions.select_parent(population, maxFitness) # select parent 2
+        child = functions.cross_over(x, y) # cross over function 
         population.append(child) # add child to population
-        if fitness(child) == maxFitness:
+        if functions.fitness(child) == maxFitness:
             break # if child has max fitness, stop
 
-        # get rid of the weakest chromosome
-        population.sort(key=lambda chromosome: fitness(chromosome), reverse=True)
+        # kill the weakest chromosome
+        population.sort(key=lambda chromosome: functions.fitness(chromosome), reverse=True)
         population = population[:population_size - 1]
         
 
